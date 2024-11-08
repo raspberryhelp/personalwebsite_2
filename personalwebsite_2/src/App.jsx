6d6eb753ed97a07fa22fe6a3adc6a05ed4e1f7a6
raspberryhelp <agnus1603@gmail.com>
2024-11-07 19:21:00 -0600

    Adding an image

--- personalwebsite_2/src/App.jsx
+++ personalwebsite_2/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import neuv from "./assets/neuv.png"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,10 @@ function App() {
     <>
       <div className='container text-center mx-auto'>
       If you're seeing this it means my app worked :)
+      </div>
+
+      <div className="columns-4">
+        <img class ="w-full aspect-video" src={neuv}/>
       </div>
       
     </>
