cbc77d8ce96f2e10717ce229d54d32ecb3eaefae
raspberryhelp <agnus1603@gmail.com>
2024-12-16 23:20:07 -0600

    Updated the nav bar so we can see a link

--- personalwebsite_2/src/App.jsx
+++ personalwebsite_2/src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import neuv from "./assets/neuv.png";
 import About from "./About";
 import Nav from "./Nav";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,15 +16,20 @@ function App() {
         name="viewport"
         content="width=device-width, initial-scale=1.0"
       ></meta>
+      <BrowserRouter>
       <div class="bg-custom-blue h-screen">
         <div className="container text-center mx-auto text-stone-100 p-10">
+         
           <Nav />
-          If you're seeing this it means my app worked :) A test to see if it
-          works even though it's already deployed
+          <Routes>
+            <Route path ="/" element={"If you're seeing this it means my app worked :) A test to see if it works even though it's already deployed"}/>
+            <Route path = "/about" element={<About/>}/>
+          </Routes>
         </div>
-        <br />
-        <About />
+       
       </div>
+      </BrowserRouter>
+ 
     </>
   );
 }
--- personalwebsite_2/src/Nav.jsx
+++ personalwebsite_2/src/Nav.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <>
-    <nav class = "bg-stone-500 rounded-t-lg p-3 m-7">This would have all my links throughout the website but nothing yet</nav>
+      <nav class="bg-stone-500 rounded-t-lg p-3 m-7">
+        <ul>
+          <ul className="flex justify-center gap-4">
+            <li>▼△▼△▼△▼△▼</li>
+          <Link to ="/about" className = "font-medium text-stone-800 hover:text-stone-50">
+        About
+        </Link>
+        <li>▼△▼△▼△▼△▼</li>
+          </ul>
+        
+        </ul>
+      
+      </nav>
     </>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
