ed423cf6be1fd37da89c7c81259d9fb9253c2a50
raspberryhelp <agnus1603@gmail.com>
2024-12-22 17:36:01 -0600

    Randomized greeting

--- personalwebsite_2/src/About.jsx
+++ personalwebsite_2/src/About.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import neuv from "./assets/neuv.png";
 
+
+
 function About() {
+
+  const greetings = [
+    "Hello there visitor! I've see you've come a long way. Here, have some tea, there's a lot to talk about...♫",
+    "Oh welcome! I just finished cleaning the kitchen, but you can have a seat right there!", 
+    "Was bringing to think that you got lost but it seems you've found me. Welcome, and please make yourself at home",
+    "Thank you for visiting my website, feel free to look around!"
+  ];
+  
+  const randomgreet = greetings[Math.floor(Math.random() * greetings.length)];
   return (
     <div className = "container mx-auto bg-stone-700 rounded-lg flex p-5 w-1/2">
         <img class ="float-left mx-auto object-cover max-h-40 rounded-lg px-10" src={neuv}/>
-        <div className="text-zinc-200">Hello! Welcome to my page! I make art sometimes but it's always sparse because of life. Enjoy my portfolio!</div>
+        <div className="text-zinc-200">{randomgreet} </div>
         
     </div>
   )
 }
 
 export default About
-
 
