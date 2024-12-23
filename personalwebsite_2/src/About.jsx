7cca80f7cc76e8cafdbe7a225eb18e4d30b774f4
raspberryhelp <agnus1603@gmail.com>
2024-12-22 18:05:34 -0600

    ok

--- personalwebsite_2/src/About.jsx
+++ personalwebsite_2/src/About.jsx
@@ -17,7 +17,6 @@ function About() {
     <div className = "container mx-auto bg-stone-700 rounded-lg flex p-5 w-1/2">
         <img class ="float-left mx-auto object-cover max-h-40 rounded-lg px-10" src={neuv}/>
         <div className="text-zinc-200">{randomgreet} </div>
-        <div className="text-zinc-200">testing </div>
         
     </div>
   )
--- personalwebsite_2/src/App.jsx
+++ personalwebsite_2/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         name="viewport"
         content="width=device-width, initial-scale=1.0"
       ></meta>
-      <BrowserRouter basename="/">
+      <BrowserRouter>
       <div class="bg-custom-blue h-screen">
         <div className="container text-center mx-auto text-stone-100 p-10">
          
