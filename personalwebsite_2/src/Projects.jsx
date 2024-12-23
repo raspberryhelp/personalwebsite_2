b2959210787b5e2bd6893e99813de6c6b521067b
raspberryhelp <agnus1603@gmail.com>
2024-12-22 20:18:14 -0600

    isk

--- personalwebsite_2/src/Projects.jsx
+++ personalwebsite_2/src/Projects.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className = "container mx-auto bg-stone-700 rounded-lg flex p-5 w-1/2">
+            <div className="text-zinc-200">  <div className="bg-custom-blue"> jfsdkfjksdaf </div>jfksfjkasjdfksadjfk </div>
+            
+        </div>
   )
 }
 
