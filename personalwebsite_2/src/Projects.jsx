5ab8d4f0b8dac8cf303165996f06f96f6fdc91c7
raspberryhelp <agnus1603@gmail.com>
2024-12-24 17:14:04 -0600

    new icon

--- personalwebsite_2/src/Projects.jsx
+++ personalwebsite_2/src/Projects.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div className = "container mx-auto bg-stone-700 rounded-lg flex p-5 w-1/2">
+    <div className = "container mx-auto bg-stone-700 rounded-lg p-5 w-1/2">
             <div className="text-zinc-200">  <div className="bg-custom-blue"> jfsdkfjksdaf </div>jfksfjkasjdfksadjfk </div>
             
         </div>
